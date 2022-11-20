15b2a506c225e750e3847a6becb76a9de3b0d5c9
soumyaRanjan <soumyaoffical95@gmail.com>
2022-11-20 11:58:17 +0530

    buttons added

--- frontend/components/ToDo.tsx
+++ frontend/components/ToDo.tsx
@@ -3,7 +3,18 @@ export default function ToDo () {
         <>
         <div className="card">
   <div className="card-body">
-    <h5 className="card-title">To Do</h5>
+    <div className="headrs">
+      <div>
+        <h5 className="card-title bi bi-plus-lg font-weight-bold text-xl-left">To Do</h5>
+      </div>
+    <div className="btn">
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-plus-lg "  viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"/>
+</svg>
+    </div>
+
+    </div>
+
     <div className="section">
     <div className="cards">
   <div className="card-header">
